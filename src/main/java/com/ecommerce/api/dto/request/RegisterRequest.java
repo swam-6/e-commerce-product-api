c95d2dbd3078f4f253b8f23e2039b98950a0088a
swam-6 <nathswami619@gmail.com>
2026-05-28 17:06:11 +0530

    Feat:add request DTO

---
+++ src/main/java/com/ecommerce/api/dto/request/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.ecommerce.api.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @Size(max = 20, message = "Phone must be at most 20 characters")
+    private String phone;
+}
