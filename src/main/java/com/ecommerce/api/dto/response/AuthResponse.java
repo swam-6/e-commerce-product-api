bacaba87eaee59cfa9792c49fe9c2b12185e4219
swam-6 <nathswami619@gmail.com>
2026-05-28 16:59:56 +0530

    Feat:add AuthResponse.java

---
+++ src/main/java/com/ecommerce/api/dto/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.ecommerce.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String role;
+    private String message;
+    private boolean success;
+}
