28af01c5af6fce169d91c34ac54404bc35917e38
swam-6 <nathswami619@gmail.com>
2026-05-28 17:10:49 +0530

    Feat:add User Details Service

---
+++ src/main/java/com/ecommerce/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ecommerce.api.security;
+
+import com.ecommerce.api.entity.User;
+import com.ecommerce.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email)
+                );
+
+        return user;
+    }
+}
